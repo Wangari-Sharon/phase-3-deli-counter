e735c1821de9ff1af650bccecea638e224297aa8
Wangari-Sharon <wangarik35@gmail.com>
2022-09-02 13:21:05 +0300

    Done

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,39 @@
 # Write your code here.
+
+require 'pry'
+
+katz_deli = []
+
+def line(array)
+    if array.length == 0
+        puts 'The line is currently empty.'
+    else
+        array_of_people = array.map.with_index do |p, index| 
+            "#{index + 1}. #{p}"
+        end
+        string = array_of_people.join(' ')
+        puts "The line is currently: #{string}"
+    end
+end
+
+def take_a_number(array, name)
+    array << name
+    number_in_line = array.length
+    puts "Welcome, #{name}. You are number #{number_in_line} in line."
+end
+
+def now_serving(array)
+    if array.length == 0
+        puts "There is nobody waiting to be served!"
+    else
+        puts "Currently serving #{array[0]}."
+        array.shift
+    end
+end
+
+puts line([])
+puts line(['Shay', 'Pat', 'Eli'])
+take_a_number(katz_deli, 'Lily')
+take_a_number(katz_deli, 'Roxy')
+take_a_number(katz_deli, 'Colleen')
+now_serving(katz_deli)
